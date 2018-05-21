0e77065a89239fcf93a61370ab7f4497e4effa67
YQS <fsaliceti@yahoo.com.ar>
2018-05-21 18:14:38 -0300

    correccion user

---
+++ src/Board.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class Board extends Component {
+  constructor() {
+    super();
+  };
+
+  componentDidMount() {
+
+  };
+
+
+render() {
+  return (
+    <div className="boards">
+    </div>
+  );
+}
+
+}
--- src/User.js
+++ src/User.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Fuse from 'fuse.js';
 
 
 export default class User extends Component {
@@ -8,7 +9,8 @@ export default class User extends Component {
       user: '',
       id: '',
       createdAt: '',
-      updatedAt: ''
+      updatedAt: '',
+      test: ''
     };
 
   };
@@ -24,28 +26,85 @@ export default class User extends Component {
   };
 
   getUser(name) {
-    fetch('http://localhost:8000/api/users/'+name, {
+    fetch('http://localhost:8000/api/users', {
       method: 'GET',
-      mode: 'cors',
-
+      mode: 'cors'
     }).then((response) => {
       if (response.status >= 400) {
         throw new Error('Bad response from server');
       }
-      //console.log(response.json());
       return response.json();
-    }).then((data) => {
-      //data es un array con dos datos
-      //  0 es el objeto
-      //  1 es un boolean que indica si tuvo que crear o no el objeto
-      console.log(data[0]);;
+    }).then((usersList) => {
+      console.log(usersList);
+      var searchUser = new Fuse(usersList, {
+        keys: ['name'],
+        threshold: 0.0,
+        id: 'id'
+      });
+      //console.log(searchUser.search(name));
+      var myId;
+      if (searchUser.search(name)[0]) {
+        console.log('found name');
+        myId = searchUser.search(name)[0];
+      } else {
+        console.log('name not found');
+        myId = name;
+      };
+      return myId
+    }).then((myId) => {
+      return fetch('http://localhost:8000/api/users/'+myId, {
+        method: 'GET',
+        mode: 'cors'
+    }).then((response) => {
+      if (response.status >= 400) {
+        throw new Error('Bad response from server');
+      }
+      return response.json();
+    }).then((foundUser) => {
+      console.log('UPDATING STATES');
+      console.log(foundUser);
       this.setState({
-        user: data[0].name,
-        id: data[0].id,
-        createdAt: data[0].createdAt,
-        updatedAt: data[0].updatedAt
+        user: foundUser.name,
+        id: foundUser.id,
+        createdAt: foundUser.createdAt,
+        updatedAt: foundUser.updatedAt,
+        test: 'ESTOY CARGADO'
       });
     })
+
+
+    })
+      /*var foundUsers = response.json().filter(function(item) {
+          return item.name === name;
+      });
+      console.log('list of users found with name ' + name);
+      console.log(foundUsers);
+      */
+      /*
+      fetch('http://localhost:8000/api/users/'+foundUsers[0].id, {
+        method: 'GET',
+        mode: 'cors',
+
+      }).then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        //console.log(response.json());
+        return response.json();
+      }).then((data) => {
+        //data es un array con dos datos
+        //  0 es el objeto
+        //  1 es un boolean que indica si tuvo que crear o no el objeto
+        console.log(data[0]);;
+        this.setState({
+          user: data[0].name,
+          id: data[0].id,
+          createdAt: data[0].createdAt,
+          updatedAt: data[0].updatedAt
+        });
+      })
+      */
+    //})
   };
 
 
@@ -56,6 +115,7 @@ export default class User extends Component {
         <p>id: {this.state.id}</p>
         <p>creado: {this.state.createdAt}</p>
         <p>actuazliado: {this.state.updatedAt}</p>
+        <p>TEST: {this.state.test} </p>
       </div>
     );
   };
